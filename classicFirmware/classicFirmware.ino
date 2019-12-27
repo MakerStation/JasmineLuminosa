b6d740c56d5e14f6ed70a55282deaec1f4ec4362
Massimo Frassetto <frassetto.massimoo@gmail.com>
2019-12-27 15:53:41 +0100

    Inserimento ciclo di avvio, alcune migliorie SW e commenti
    Inserito un semplicissimo ciclo di avvio per non avere subito tutti i led accessi in caso di steadyMode.
    Inserite alcune migliorie sw per rendere più "affidabile" e leggibile il sorgente.
    Inseriti alcuni commenti.

--- classicFirmware/classicFirmware.ino
+++ classicFirmware/classicFirmware.ino
@@ -5,8 +5,13 @@ Esse sono composta da LED. Ogni lettera è gestita da un solo relè
 // alt+254
 // alt+176
 
+/*Commentare la successiva riga se si vogliono escludere tutte le righe di DEBUG.
+NOTA: purtroppo in questa versione c'è un problema che se si escludono tutte le righe di serial.print entra in modalità waveMode e da lì non esce.
+Considerando i tempi di consegna e che non inficiano o rallentano il fw ho deciso di lasciarlo sempre attivo. Vedrò poi se spendere ulteriore tempo in ricerca del problema.
+*/
 #define DEBUG
 
+//rapida impostazione per cambiare la logica di controllo a seconda che stia utilizzando dei led o i rele: "1" per i led; "0" per i rele
 #define testLed 1
 
 //numero lettere
@@ -20,34 +25,34 @@ Esse sono composta da LED. Ogni lettera è gestita da un solo relè
 #define letterIPin 7
 #define letterNPin 8
 #define letterEPin 9
-
 //pin per selzionare la modalità
 #define modeSelectorPin 10
+
 //modalità di lavoro del pin del selettore
-#define modeSelectorMode INPUT
+#define readingModeSelectorModePin INPUT
 //modalità di lavoro dei pin delle lettere
 #define letterPinMode OUTPUT
 //durata accensione di una lettera (ms)
 #define defaultTimePerLetter 800
-
+//definisco quale numero corrisponde a quale modalità.
 #define steadyMode 0
 #define waveMode 1
-
+//preparo il mio array di lettere. 
 const int letterPin[letterNumber]= {letterJPin,letterAPin,letterSPin,letterMPin,letterIPin,letterNPin,letterEPin};
 
-//variabile di stato per capire in che modalità mi trovo
+//variabile di stato per capire in che modalità mi trovavo
+int previusMode=0;
+//variabile di stato per capire in che modalità mi trovo attualmente
 int currentMode=0;
 //variabile per capire la nuova modalità selezionata
 int selectedMode=0;
-//variabile per capire se lo stato è cambiato.
-int modeChanged=0;
 //varibile per capire quando il ciclo è incominciato
 unsigned long startCycleTime=0;
 //variabile per capire a che punto sono del ciclo
 unsigned long elapsedCycleTime=0;
 //variabile per modulare le condizioni temporali in base alla lettera che sto controllando
 int letterIndex=0;
-
+//semplice inizializzazione della variabile. nel setup decido che valore deve avere.
 int lightON=0;
 
 unsigned long timePerLetter=defaultTimePerLetter;
@@ -55,23 +60,33 @@ unsigned long timePerLetter=defaultTimePerLetter;
 //=================================INIZIO SKETCH=================================
 void setup() {
 	#ifdef DEBUG
-	Serial.begin(2000000);
+		Serial.begin(2000000);
+		Serial.print("STARTING SYSTEM");
 	#endif
 	//imposto i pin secondo le modalità scelte
-    for (letterIndex;letterIndex<letterNumber+1;letterIndex++){
+    for (letterIndex;letterIndex<=letterNumber;letterIndex++){
         pinMode(letterPin[letterIndex], letterPinMode);
     }
     letterIndex=0;
-    pinMode(modeSelectorPin, modeSelectorMode);
+    pinMode(modeSelectorPin, readingModeSelectorModePin);
     //leggo subito lo stato del selettore
     selectedMode=digitalRead(modeSelectorPin);
-	
+	//decido se devo controllare i rele (LOW=accesi) o se devo accendere direttamente i led (HIGH=accesi)
 	if(testLed){
 		lightON=1;
 	}
 	else if(!testLed){
 		lightON=0;
 	}
+	//semplice accensione iniziale per non avere la stangata di corrente subito.
+	for (letterIndex;letterIndex<=letterNumber;letterIndex++){
+		delay(200);
+		digitalWrite(letterPin[letterIndex], lightON);
+		#ifdef DEBUG
+			Serial.print(".");
+		#endif
+	}
+	letterIndex=0;
 }
 
 void loop() {
@@ -84,23 +99,25 @@ void loop() {
 		Serial.print(selectedMode);
 		Serial.print("/");
         Serial.print(currentMode);
+		Serial.print("/");
+        Serial.print(previusMode);
         Serial.print(" - LED:");
 	#endif
     //scelgo che modalità utilizzare a seconda dello stato del selettore
-	if(selectedMode==steadyMode){
+	if(currentMode==steadyMode){
         //nella prima modalità vado ad accendere TUTTE le lettere
-		for (letterIndex;letterIndex<letterNumber+1;letterIndex++){
+		elapsedCycleTime=0;
+		for (letterIndex;letterIndex<=letterNumber;letterIndex++){
 			digitalWrite(letterPin[letterIndex], lightON);
 			#ifdef DEBUG
 				Serial.print("*");
-				elapsedCycleTime=0;
 			#endif
 		}
 		letterIndex=0;
 	}
-	else if(selectedMode==waveMode){
+	else if(currentMode==waveMode){
 		//nella seconda modalità vado a prima a varificare se devo ricominciare il ciclo (2 casi: ciclo iniziato per la prima volta OPPURE ciclo terminato, per cui devo ricominciare) 
-		if((selectedMode!=currentMode)||(elapsedCycleTime>(timePerLetter*(letterNumber*2)))){
+		if((currentMode!=previusMode)||(elapsedCycleTime>(timePerLetter*(letterNumber*2)))){
 			//resetto il tempo di inizio ed il timer
 			startCycleTime=millis();
 			elapsedCycleTime=0;
@@ -108,7 +125,7 @@ void loop() {
 		//aggiorno lo stato del timer
 		elapsedCycleTime=millis()-startCycleTime;
 		//vado ad attivare le lettere se sono all'interno del range temporale di funzionamento
-		for (letterIndex;letterIndex<letterNumber;letterIndex++){
+		for (letterIndex;letterIndex<=letterNumber;letterIndex++){
 			if(((elapsedCycleTime>timePerLetter*(letterIndex+1)))&&(elapsedCycleTime<(timePerLetter*(letterNumber+letterIndex+1)))){
 				digitalWrite(letterPin[letterIndex], lightON); //
 				#ifdef DEBUG
@@ -129,9 +146,11 @@ void loop() {
 		Serial.print(" | ");
 		Serial.print((unsigned long)(timePerLetter*(letterNumber*2+1)));
 	#endif
-	//salvo in che modalità sono attualmente
-    currentMode=selectedMode;
+	//salvo in che modalità ero
+	previusMode=currentMode;
     //leggo il selettore per capire se dovrò cambiare modalità
 	selectedMode=digitalRead(modeSelectorPin);
-    //selectedMode=1;
+    currentMode=selectedMode;
+	//delay(100);
+    //selectedMode=0;
 }
